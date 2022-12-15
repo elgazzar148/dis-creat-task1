3b759900485d84cda0bf227a04f145fa7d9ae721
elgazzar148 <120646390+elgazzar148@users.noreply.github.com>
2022-12-15 13:45:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pleas inter the n1");
+            double N1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("pleas inter the n2");
+            double N2 = double.Parse(Console.ReadLine());
+
+            bool prime;
+
+            for (double i = N1; i < N2; i++)
+            {
+                prime = true;
+
+                if (i == 1)
+                {
+                    prime = false;
+
+                }
+
+                for (double j = 2; j < N1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime)
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+            }
+    }
+}
